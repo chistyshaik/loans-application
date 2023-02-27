69d212ab294734aed4decb9f30617b12ee35d187
Shaik Chisty <shaikchisty8@gmail.com>
2023-02-27 13:50:38 +0530

    loan-application part-3 updation

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ app.get('/', (req, res) => {
 app.get('/loans', (req, res) => {
 
     db.serialize(() => {
-        db.all(`SELECT * from loans `, (error, rows) => {
+        const selectQuery = `SELECT * from loans `
+        db.all(selectQuery, (error, rows) => {
             if (error) {
                 res.json({
                     status: false,
@@ -90,15 +91,90 @@ app.post('/new-loan', (req, res) => {
         return sendErrorResponse(res, 'Please provide purpose');
     };
 
+    const insertSQL = `INSERT INTO loans(
+        firstName,
+        lastName,
+        email,
+        loan_amount,
+        purpose
+    ) VALUES(
+        "${firstName}",
+        "${lastName}",
+        "${email}",
+        "${amount}",
+        "${purpose}"
+    )`
+
+    db.serialize(() => {
+        db.exec(insertSQL, (error) => {
+            if (error) {
+                res.status(400).json({
+                    status: false,
+                    error: error
+                });
+            }
+        })
+    });
+
     res.json({
         status: true,
         method: "post",
         data: loanData,
-        message: "new loan application created successfully"
+        message: "new loan application created successfully",
+        sql: insertSQL
+    });
+});
+
+app.get('/new-loan/:id', (req, res) => {
+    const loan_id = req.params.id;
+
+    const sql = `SELECT * FROM loans WHERE loan_id=${loan_id} ;`;
+    db.serialize(() => {
+        //get (sql:string , callback? (this: statement, err:Error | null, row: any) => void): this;
+        db.get(sql, (err, row) => {
+            if (err || !row) {
+                res.status(400).json({ status: false, error: `unable to load application with ${loan_id}` })
+            } else {
+                res.json({ status: true, loan: row })
+            };
+        });
     });
 });
 
 //in the above code we have repeated the same code 5 times so rather than writing it multiple the same piece of code multiple times we can use a function.
+
+app.post('/new-loan/:id', (req, res) => {
+    const loan_id = req.params.id;
+    const requestBody = req.body;
+    const status = requestBody.status;
+
+
+    const sql = `
+    UPDATE loans
+    SET status = "${status}"
+    WHERE loan_id= ${loan_id}
+    `;
+
+    db.serialize(() => {
+        db.exec(sql, (error) => {
+            console.log(error)
+            if (error) {
+                res.status(400).json({
+                    status: false,
+                    sql,
+                    error: `Error while updating the loan for id ${loan_id}`
+                })
+            }else {
+                res.json({
+                    status: true,
+                    message: "loan details updated successfully"
+                })
+            }
+        })
+    })
+
+});
+
 
 function sendErrorResponse(response, errorMessage) {
     return response.status(400).json({
